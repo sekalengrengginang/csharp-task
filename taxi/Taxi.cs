811c403a07e32c6343a79fe1dffa68aafd325f46
jun <ahmadhawari@students.amikom.ac.id>
2022-03-23 17:47:07 +0700

    still working on class

---
+++ taxi/Taxi.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace taxiProgram{
+    public class Taxi{
+        public string DriverName {get; set;}
+        public string OnDuty {get; set;}
+        public string NumPassenger {get; set;}
+    }
+
+            public void Taxi(){
+
+                Console.WriteLine("Informasi Taksi: {0}",TaxiInfo);
+                Console.WriteLine("Penumpang Naik:{0} ",PickUpPassenger);
+                Console.WriteLine("Penumpang Turun: {0} ",DropOffPassenger;
+
+
+
+
+
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
