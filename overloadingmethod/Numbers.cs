cda723e3191230166638b60dd55b05b3892bebb6
jun <ahmadhawari@students.amikom.ac.id>
2022-04-07 16:33:07 +0700

    fixing something

--- overloadingmethod/Numbers.cs
+++ overloadingmethod/Numbers.cs
@@ -3,13 +3,16 @@ public class Numbers
     public int FindMinimum(int number1,int number2)
     {
     int jumlahmin;
-        if (number1 < number2){
+
+    if (number1 < number2){
             jumlahmin = number1;
         }
-        else {
+    else {
           jumlahmin = number2;
         }
-        return jumlahmin; 
+
+    return jumlahmin;
+
     }
     public int FindMinimum(int number1,int number2,float number3)
     {
@@ -28,29 +31,29 @@ public class Numbers
 }
     public int FindMaximum(int number1,int number2)
     {
-    int jumlahmin;
+    int jumlahmax;
         if (number1 > number2){
-            jumlahmin = number1;
+            jumlahmax = number1;
         }
         else {
-          jumlahmin = number2;
+          jumlahmax = number2;
         }
-        return jumlahmin; 
+        return jumlahmax; 
     }
     public int FindMaximum(int number1,int number2,float number3)
     {
-    int jumlahmin;
+    int jumlahmax;
 
     if(number1 > number2 && number1 > number3){
-         jumlahmin = number1;
+         jumlahmax = number1;
       }  
     else if (number2 > number1 && number2 > number3){
-         jumlahmin = number2;
+         jumlahmax = number2;
     }  
     else {
-        jumlahmin = (int)number3;
+        jumlahmax = (int)number3;
     }
-    return jumlahmin; 
+    return jumlahmax; 
 }
  
 
--- overloadingmethod/Program.cs
+++ overloadingmethod/Program.cs
@@ -7,17 +7,9 @@ static void Main(string[]args){
 
     Console.WriteLine("Minimum #1: {0}",number.FindMinimum(3,1));
     Console.WriteLine("Minimum #2: {0}",number.FindMinimum(3,2,4));
-    Console.WriteLine("Minimum #2: {0}",number.FindMaximum(3,1));
-    Console.WriteLine("Minimum #2: {0}",number.FindMaximum(3,2,4));
-
-
+    Console.WriteLine("Minimum #3: {0}",number.FindMaximum(3,1));
+    Console.WriteLine("Minimum #4: {0}",number.FindMaximum(3,2,4));
     Console.ReadKey();
-
-
-
-
-
-
 }
 
 
