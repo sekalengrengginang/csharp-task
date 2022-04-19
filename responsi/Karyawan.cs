688c04fbd99ae867fa893d149b6e29d4fcc301c3
jun <ahmadhawari@students.amikom.ac.id>
2022-04-19 16:47:01 +0700

    fix something

--- responsi/Karyawan.cs
+++ responsi/Karyawan.cs
@@ -3,12 +3,12 @@ namespace karyawan {
 
 //properties
 public class Karyawan{
-    private string  Nik 
-    private string Nama  
-    private int GajiBulanan 
+    public string  Nik 
+    public string Nama  
+    public int GajiBulanan 
 
 
-    public string karyawanSatu(){
+    public string Nik(){
         Console.WriteLine( "{0}",Nama);
         Console.WriteLine( "{0}",Nik);
         Console.WriteLine( "{0}",GajiBulanan);
