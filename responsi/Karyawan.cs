c3e448f4a65ae1be1ea3e8ff1ad0f2a54ba2abc0
jun <ahmadhawari@students.amikom.ac.id>
2022-04-19 16:27:17 +0700

    add responsi

---
+++ responsi/Karyawan.cs
@@ -0,0 +1,71 @@
+using System;
+namespace karyawan {
+
+//properties
+public class Karyawan{
+    private string  Nik 
+    private string Nama  
+    private int GajiBulanan 
+
+
+    public string karyawanSatu(){
+        Console.WriteLine( "{0}",Nama);
+        Console.WriteLine( "{0}",Nik);
+        Console.WriteLine( "{0}",GajiBulanan);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ responsi/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ResponsiPemrograman4296{
+public class ResponsiPemrograman4296{
+
+ 
+    static void Main(string[]args){
+
+        //constructor
+        Karyawan karyawanSatu = new Karyawan("190302123","Paijo",4000000);
+        
+        karyawan karyawanDua = new Karyawan("190302124", "Jono", 2000000);
+        
+
+    Console.WriteLine("No Nik/Nama               Gaji Bulanan");
+    Console.WriteLine("-------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
