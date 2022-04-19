fa2e986fe6933f7eebdb00e1ecc76002c2668454
jun <ahmadhawari@students.amikom.ac.id>
2022-04-19 16:48:24 +0700

    fix something (2)

--- responsi/Program.cs
+++ responsi/Program.cs
@@ -5,9 +5,9 @@ public class ResponsiPemrograman4296{
     static void Main(string[]args){
 
         //constructor
-        Karyawan karyawanSatu = new Karyawan("190302123","Paijo",4000000);
+        Karyawan karyawanSatu = new Karyawan();
         
-        karyawan karyawanDua = new Karyawan("190302124", "Jono", 2000000);
+        karyawan karyawanDua = new Karyawan();
         
 
     Console.WriteLine("No Nik/Nama               Gaji Bulanan");
